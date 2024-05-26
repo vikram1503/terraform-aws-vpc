ba18e00ff39f50c3af56a88b63e60ee513eb3c46
vikram1503 <vikram15031989@gmail.com>
2024-05-26 09:19:01 +0530

    practice

--- outputs.tf
+++ outputs.tf
@@ -27,3 +27,13 @@ output "db_subnet_group_id" {
     value = aws_db_subnet_group.default.id
   
 }
+
+output "db_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+  
+}
+
+output "igw_id" {
+    value = aws_internet_gateway.gw.id
+  
+}
